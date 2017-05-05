c6479dacd6d41f2e0e470c58c6fab53a9493fd93
OptimisticShibe <britishdragons123@gmail.com>
2017-05-04 21:04:04 -0700

    Updating from class session #9

---
+++ src/Ziggle.Repository/DatabaseAccessor.cs
@@ -0,0 +1,23 @@
+﻿using Ziggle.ProductDatabase;
+
+namespace Ziggle.Repository
+{
+    public class DatabaseAccessor
+    {
+        private static readonly ProductDbEntities entities;
+
+        static DatabaseAccessor()
+        {
+            entities = new ProductDbEntities();
+            entities.Database.Connection.Open();
+        }
+
+        public static ProductDbEntities Instance // Static b/c we don't want to create an instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
--- src/Ziggle.WebSite/Controllers/HomeController.cs
+++ src/Ziggle.WebSite/Controllers/HomeController.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ziggle.Business;
+using Ziggle.WebSite.Models;
 
 namespace Ziggle.WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICategoryManager categoryManager;
+        private readonly IProductManager productManager;
+        private readonly IUserManager userManager;
+
+        public HomeController(ICategoryManager categoryManager, IProductManager productManager,
+            IUserManager userManager)
+        {
+            this.categoryManager = categoryManager;
+            this.productManager = productManager;
+            this.userManager = userManager;
+        }
+
+        public ActionResult Category(int id)
+        {
+            var category = categoryManager.Category(id);
+            var products = productManager
+                                .ForCategory(id)
+                                .Select(t =>
+                                    new Ziggle.WebSite.Models.ProductModel
+                                    {
+                                        Id = t.Id,
+                                        Name = t.Name,
+                                        Price = t.Price,
+                                        Quantity = t.Quantity
+                                    }).ToArray();
+
+            var model = new CategoryViewModel
+            {
+                Category = new Ziggle.WebSite.Models.CategoryModel(category.Id, category.Name),
+                Products = products
+            };
+
+            return View(model);
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var categories = categoryManager.Categories
+                                            .Select(t => new Ziggle.WebSite.Models.CategoryModel(t.Id, t.Name))
+                                            .ToArray();
+            var model = new IndexModel { Categories = categories };
+            return View(model);
         }
 
         public ActionResult About()
@@ -25,6 +66,43 @@ namespace Ziggle.WebSite.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LoginModel loginModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.LogIn(loginModel.UserName, loginModel.Password);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "User name and password do not match.");
+                }
+                else
+                {
+                    Session["User"] = new Ziggle.WebSite.Models.UserModel { Id = user.Id, Name = user.Name };
+
+                    System.Web.Security.FormsAuthentication.SetAuthCookie(loginModel.UserName, false);
+
+                    return Redirect(returnUrl ?? "~/");
+                }
+            }
+
+            return View(loginModel);
+        }
+
+        public ActionResult LogOff()
+        {
+            Session["User"] = null;
+            System.Web.Security.FormsAuthentication.SignOut();
+
+            return Redirect("~/");
         }
     }
 }
